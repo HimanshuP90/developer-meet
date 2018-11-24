9944e8ce36821d2e928216183b78744fc33d268d
Himanshu Pandey <himanshup895@gmail.com>
2018-11-24 16:29:37 +0530

    create profile added

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from './types';
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Get current profile
 export const getCurrentPorfile = () => dispatch => {
@@ -20,6 +20,40 @@ export const getCurrentPorfile = () => dispatch => {
         );
 }
 
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+    axios
+        .post('/api/profile', profileData)
+        .then(res => history.push('/dashboard'))
+        .catch(err =>
+            dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data  
+            })
+        );
+}
+
+// Delete account & profile
+export const deleteAccount = () => dispatch => {
+    if(window.confirm('Are you sure? This can NOT be undone!')) {
+        axios
+            .delete('/api/profile')
+            .then(res => 
+                dispatch({
+                    type: SET_CURRENT_USER,
+                    payload: {}
+                })
+            )
+            .catch(err =>
+                dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data  
+                })
+            );
+    }
+}
+
+
 // Profile Loading
 export const setProfileLoading = () => {
     return {
@@ -33,3 +67,4 @@ export const clearCurrentProfile = () => {
         type: CLEAR_CURRENT_PROFILE,
     }
 }
+
