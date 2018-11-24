d13af07be08e4758082f2367e526ac2a6175fd63
Himanshu Pandey <himanshup895@gmail.com>
2018-11-24 09:46:29 +0530

    added common for login & register

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -47,3 +47,13 @@ export const setCurrentUser = decoded => {
       payload: decoded
     };
   };
+
+  // Log user out
+export const logoutuser = () => dispatch => {
+  // Remove token from localstorage
+  localStorage.removeItem('jwtToken');
+  // Remove auth header for future request
+  setAuthToken(false);
+  // Set current user to {} which will set isAuthenticated to false
+  dispatch(setCurrentUser({}))
+}
